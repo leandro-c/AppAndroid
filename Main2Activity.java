860faa9956f8a76608f41b86b5aed9c31d894962
leandro.casarin <leandroccasarin@gmai.com>
2016-11-22 13:05:39 -0300

    first commit

---
+++ Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.leandrocasarin.apptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        String s = getIntent().getExtras().getString("key");
+        Toast.makeText(Main2Activity.this, s, Toast.LENGTH_SHORT).show();
+
+    }
+}
